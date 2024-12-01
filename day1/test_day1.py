c19456b8c6421b7c0b6fadc0928ca8ca741c778b
Dominique Bruneau <druneau@gmail.com>
2024-12-01 08:35:51 -0500

    changed input paths to run ptw from inside day folders

--- day1/test_day1.py
+++ day1/test_day1.py
@@ -11,13 +11,13 @@ def test_distance():
 
 def test_sum_distance():
     assert day1.sum_distance_tuples([(3, 1), (4, 6)]) == (2 + 2)
-    example = day1.read_file_as_tuples("./day1/input_example.txt")
+    example = day1.read_file_as_tuples("input_example.txt")
     sorted_example = day1.sort_tuple_halves(example)
     assert day1.sum_distance_tuples(sorted_example) == 11
 
 
 def test_read_file_as_tuples():
-    assert day1.read_file_as_tuples("./day1/input_example.txt") == [
+    assert day1.read_file_as_tuples("input_example.txt") == [
         (3, 4),
         (4, 3),
         (2, 5),
@@ -32,5 +32,5 @@ def test_sort_tuple_halves():
 
 
 def test_part1():
-    assert day1.part1(input="./day1/input_example.txt") == 11
-    assert day1.part1(input="./day1/input.txt") == 3246517
+    assert day1.part1(input="input_example.txt") == 11
+    assert day1.part1(input="input.txt") == 3246517
