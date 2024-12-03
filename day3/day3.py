997617f32c9f8e4f27f04aa7ebfde9952d80c9ea
Dominique Bruneau <druneau@gmail.com>
2024-12-03 06:36:14 -0500

    add solved day3 part2

--- day3/day3.py
+++ day3/day3.py
@@ -2,19 +2,43 @@ import re
 import tools.file
 
 PATTERN_MUL = r"mul\(\d+,\d+\)"
+PATTERN_INSTRUCTIONS = r"mul\(\d+,\d+\)|do\(\)|don\'t\(\)"
+
+ENABLED = True
 
 
 def get_muls(line):
     return re.findall(PATTERN_MUL, line)
 
 
-def execute_mul(instruction):
-    match = re.search(r"(\d+),(\d+)", instruction)
-    num1, num2 = map(int, match.groups())
-    return num1 * num2
+def get_instructions(line):
+    return re.findall(PATTERN_INSTRUCTIONS, line)
 
 
-def run_program(filename):
+def execute_instruction(instruction):
+    global ENABLED
+    if instruction == "do()":
+        ENABLED = True
+    elif instruction == "don't()":
+        ENABLED = False
+    else:
+        if ENABLED:
+            match = re.search(r"(\d+),(\d+)", instruction)
+            num1, num2 = map(int, match.groups())
+            return num1 * num2
+
+    return 0
+
+
+def part1(filename):
     lines = tools.file.read_file_as_strings(filename)
     instructions = [instruction for line in lines for instruction in get_muls(line)]
-    return sum(execute_mul(instruction) for instruction in instructions)
+    return sum(execute_instruction(instruction) for instruction in instructions)
+
+
+def part2(filename):
+    lines = tools.file.read_file_as_strings(filename)
+    instructions = [
+        instruction for line in lines for instruction in get_instructions(line)
+    ]
+    return sum(execute_instruction(instruction) for instruction in instructions)
--- day3/test_day3.py
+++ day3/test_day3.py
@@ -1,6 +1,7 @@
 import day3
 
 INPUT_EXAMPLE = "day3/input_example.txt"
+INPUT_EXAMPLE_PART2 = "day3/input_example_part2.txt"
 INPUT = "day3/input.txt"
 
 
@@ -11,11 +12,28 @@ def test_regex_match_mul():
     assert day3.get_muls("mul(1,2)mul(1,2))(@*#)") == ["mul(1,2)", "mul(1,2)"]
 
 
-def test_execute_mul():
-    assert day3.execute_mul("mul(1,2)") == 2
-    assert day3.execute_mul("mul(11,8)") == 88
+def test_execute_instruction():
+    assert day3.execute_instruction("mul(1,2)") == 2
+    assert day3.execute_instruction("mul(11,8)") == 88
 
 
-def test_run_program():
-    assert day3.run_program(INPUT_EXAMPLE) == 161
-    assert day3.run_program(INPUT) == 180233229
+def test_part1():
+    assert day3.part1(INPUT_EXAMPLE) == 161
+    assert day3.part1(INPUT) == 180233229
+
+
+def test_get_instructions():
+    assert day3.get_instructions("do()") == ["do()"]
+    assert day3.get_instructions("don't()") == ["don't()"]
+    assert day3.get_instructions("mul(1,2)do()don't()mul(1,2)") == [
+        "mul(1,2)",
+        "do()",
+        "don't()",
+        "mul(1,2)",
+    ]
+
+
+def test_part2():
+    assert day3.part2(INPUT_EXAMPLE) == 161
+    assert day3.part2(INPUT_EXAMPLE_PART2) == 48
+    assert day3.part2(INPUT) == 95411583
