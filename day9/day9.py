dc28085a519f02846de1f3db5ff0d5aa836fcbd6
Dominique Bruneau <druneau@gmail.com>
2024-12-09 15:35:46 -0500

    add solved day9 part2

--- day9/day9.py
+++ day9/day9.py
@@ -1,5 +1,7 @@
 from collections import deque
 from tools.file import read_file_as_string
+from itertools import chain
+import time
 
 
 def to_blocks(disk_map: list[int]) -> list[int | str]:
@@ -17,7 +19,7 @@ def to_blocks(disk_map: list[int]) -> list[int | str]:
     return blocks
 
 
-def compact(blocks):
+def per_block_compact(blocks):
     compacted_blocks = []
     free_space_count = 0
 
@@ -41,12 +43,117 @@ def compact(blocks):
     return compacted_blocks
 
 
-def checksum(compacted_blocks: list[int]) -> int:
-    return sum(index * block for index, block in enumerate(compacted_blocks))
+def blocks_to_files(blocks):
+    files = []
+
+    file_data = None
+    file_length = 0
+    file_start_index = 0
+
+    for index, block in enumerate(blocks):
+        if file_data is None:
+            file_start_index = index
+            file_length = 1
+            file_data = block
+        elif block == file_data:
+            file_length += 1
+        else:
+            files.append((file_start_index, file_length, file_data))
+            file_data = block
+            file_start_index = index
+            file_length = 1
+
+    files.append((file_start_index, file_length, file_data))
+
+    return files
+
+
+def files_to_blocks(files):
+    blocks = []
+
+    for _, length, data in files:
+        blocks.extend([data] * length)
+    return blocks
+
+
+def insert_file(file, free_space):
+    file_id, length, data = file
+    free_id, free_length, free_data = free_space
+
+    if length <= free_length:
+        extra_free_space = (free_id + length, free_length - length, free_data)
+        freed_space = (file_id, length, free_data)
+        free = [extra_free_space, freed_space]
+        file = (free_id, length, data)
+    else:
+        free = [free_space]
+
+    return file, free
+
+
+def per_file_compact(files):
+    files_set = set(files)
+    compacted_set = set()
+
+    while files_set:
+        # get las file
+        file = max(files_set, key=lambda f: f[0])
+
+        index, length, data = file
+
+        if data == ".":
+            compacted_set.add(file)
+            files_set.remove(file)
+        else:
+            space = find_space(length, files_set)
+            if space:
+                files_set.remove(file)
+                file, space_fragments = insert_file(file, space)
+
+                space_left, space_used = chain(space_fragments, [])
+
+                compacted_set.add(file)
+
+                compacted_set.add(space_used)
+                files_set.remove(space)
+                files_set.add(space_left)
+
+            else:
+                files_set.remove(file)
+                compacted_set.add(file)
+
+    compacted_files = sorted(compacted_set, key=lambda f: f[0])
+    blocks = files_to_blocks(compacted_files)
+    return blocks
+
+
+def find_space(space_length, files):
+    available_spaces = [f for f in files if f[1] >= space_length and f[2] == "."]
+
+    if available_spaces:
+        return min(available_spaces, key=lambda f: f[0])
+    else:
+        return None
+
+
+def checksum(compacted_blocks: list[int | str]) -> int:
+    return sum(
+        index * block
+        for index, block in enumerate(compacted_blocks)
+        if isinstance(block, int)
+    )
 
 
 def part1(filepath):
     disk_map = read_file_as_string(filepath)
     blocks = to_blocks(disk_map)
-    compacted_blocks = compact(blocks)
+    compacted_blocks = per_block_compact(blocks)
+    return checksum(compacted_blocks)
+
+
+def part2(filepath):
+    disk_map = read_file_as_string(filepath)
+    blocks = to_blocks(disk_map)
+    files = blocks_to_files(blocks)
+    compacted_blocks = per_file_compact(files)
     return checksum(compacted_blocks)
--- day9/test_day9.py
+++ day9/test_day9.py
@@ -15,9 +15,9 @@ def test_to_blocks():
     ]
 
 
-def test_compact():
+def test_per_block_compact():
     # fmt:off
-    assert day9.compact([0,0,".",".",".",1,1,1,".",".",".",2,".",
+    assert day9.per_block_compact([0,0,".",".",".",1,1,1,".",".",".",2,".",
                          ".",".",3,3,3,".",4,4,".",5,5,5,5,".",6,
                          6,6,6,".",7,7,7,".",8,8,8,8,9,9,]
                          
@@ -35,3 +35,50 @@ def test_checksum():
             [0,0,9,9,8,1,1,1,8,8,8,2,7,7,7,3,3,3,6,4,4,6,5,5,5,5,6,6,]
         ) == 1928
     )
+
+
+def test_blocks_to_files():
+    assert day9.blocks_to_files([1, 2, 3]) == ([(0, 1, 1), (1, 1, 2), (2, 1, 3)])
+    assert day9.blocks_to_files([1, 2, 3, 3]) == ([(0, 1, 1), (1, 1, 2), (2, 2, 3)])
+    assert day9.blocks_to_files([1, ".", 3, 3]) == ([(0, 1, 1), (1, 1, "."), (2, 2, 3)])
+    assert day9.blocks_to_files([".", ".", ".", "."]) == ([(0, 4, ".")])
+    assert day9.blocks_to_files([".", ".", 1, "."]) == (
+        [(0, 2, "."), (2, 1, 1), (3, 1, ".")]
+    )
+
+
+def test_files_to_blocks():
+    assert day9.files_to_blocks([(0, 1, 1), (1, 1, 2), (2, 1, 3)]) == [1, 2, 3]
+    assert day9.files_to_blocks(([(0, 4, ".")])) == [".", ".", ".", "."]
+
+
+def test_insert_file():
+    assert day9.insert_file((2, 1, 3), (0, 4, ".")) == (
+        (0, 1, 3),
+        [(1, 3, "."), (2, 1, ".")],
+    )
+    assert day9.insert_file((99, 10, 1), (0, 4, ".")) == ((99, 10, 1), [(0, 4, ".")])
+
+
+def test_find_space():
+    blocks = day9.to_blocks("2333133121414131402")
+    files = day9.blocks_to_files(blocks)
+    assert day9.find_space(4, [(0, 4, ".")]) == (0, 4, ".")
+    assert day9.find_space(3, [(0, 4, ".")]) == (0, 4, ".")
+    assert day9.find_space(5, [(0, 4, ".")]) is None
+    assert day9.find_space(3, files) == (2, 3, ".")
+
+
+def test_per_file_compact():
+    # fmt: off
+    blocks = day9.to_blocks("2333133121414131402")
+    files = day9.blocks_to_files(blocks)
+    assert day9.per_file_compact(files) == [0, 0, 9, 9, 2, 1, 1, 1, 7, 7, 7, '.', 4, 4,
+                                             '.', 3, 3, 3, '.', '.', '.', '.', 5, 5, 5, 5,
+                                               '.', 6, 6, 6, 6, '.', '.', '.', '.', '.',
+                                                 8, 8, 8, 8, '.', '.']
+
+
+def test_part2():
+    assert day9.part2("day9/input_example.txt") == 2858
+    assert day9.part2("day9/input.txt") == 6321896265143
