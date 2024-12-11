ced2733427d50c3994baf1c43f1a1d2b31a8fae3
Dominique Bruneau <druneau@gmail.com>
2024-12-11 17:31:13 -0500

    add day11 part2 solution ( I had to lookup a hint...)

--- day11/day11.py
+++ day11/day11.py
@@ -12,21 +12,46 @@ def blink(stone):
     return [stone * 2024]
 
 
-def blink_all(stones, blink_count=1):
-    for i in range(blink_count):
-        if i == 0:
-            after_blink = []
-        else:
-            stones = after_blink
-            after_blink = []
+def recursive_blink_count(stone, blinks_left):
+    if blinks_left == 0:
+        return 1
 
-        for stone in stones:
-            after_blink.extend(blink(stone))
+    count = 0
+    new_stones = blink(stone)
+    for new_stone in new_stones:
+        count += recursive_blink_count(new_stone, blinks_left - 1)
 
-    return after_blink
+    return count
+
+
+def blink_as_dict(stones: dict) -> dict:
+    stones_tally = {}
+
+    for value, count in stones.items():
+        new_stones = blink(value)
+
+        for new_stone in new_stones:
+            stones_tally[new_stone] = stones_tally.get(new_stone, 0) + count
+    return stones_tally
 
 
 def part1(filepath):
     stones = read_file_as_lists(filepath)[0]
-    print(f"stones:{stones}")
-    return len(blink_all(stones, 25))
+    total_count = 0
+    for stone in stones:
+        total_count += recursive_blink_count(stone, blinks_left=25)
+
+    return total_count
+
+
+def part2(filepath, blinks):
+    stones = read_file_as_lists(filepath)[0]
+
+    stones_tally = {}
+    for stone in stones:
+        stones_tally[stone] = 1
+
+    for _ in range(blinks):
+        stones_tally = blink_as_dict(stones_tally)
+
+    return sum(stones_tally.values())
--- day11/test_day11.py
+++ day11/test_day11.py
@@ -11,35 +11,13 @@ def test_blink():
     assert day11.blink(999) == [999 * 2024]
 
 
-def test_blink_all():
-    assert day11.blink_all([125, 17]) == [253000, 1, 7]
-    assert day11.blink_all([125, 17], blink_count=2) == [253, 0, 2024, 14168]
-    assert day11.blink_all([125, 17], blink_count=6) == [
-        2097446912,
-        14168,
-        4048,
-        2,
-        0,
-        2,
-        4,
-        40,
-        48,
-        2024,
-        40,
-        48,
-        80,
-        96,
-        2,
-        8,
-        6,
-        7,
-        6,
-        0,
-        3,
-        2,
-    ]
-
-
 def test_part1():
     assert day11.part1("day11/input_example.txt") == 55312
     assert day11.part1("day11/input.txt") == 204022
+
+
+def test_part2():
+    assert day11.part2("day11/input_example.txt", 6) == 22
+    assert day11.part2("day11/input_example.txt", 25) == 55312
+    assert day11.part2("day11/input.txt", 25) == 204022
+    assert day11.part2("day11/input.txt", 75) == 241651071960597
