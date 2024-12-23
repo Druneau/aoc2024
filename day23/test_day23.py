716ae34e960a19f6cf7c4f298b90228abc1015b4
Dominique Bruneau <druneau@gmail.com>
2024-12-23 06:12:21 -0500

    fix wrong test assert value

--- day23/test_day23.py
+++ day23/test_day23.py
@@ -8,4 +8,4 @@ def test_part1():
 
 def test_part2():
     assert day23.part2("day23/input_example.txt") == "co,de,ka,ta"
-    assert day23.part2("day23/input.txt") == "co,de,ka,ta"
+    assert day23.part2("day23/input.txt") == "bs,ey,fq,fy,he,ii,lh,ol,tc,uu,wl,xq,xv"
