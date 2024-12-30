d220707d18a31c54d7fda300f93fbb446cc3ad73
Dominique Bruneau <druneau@gmail.com>
2024-12-30 13:42:00 -0500

    attempt day16 part2 via recursion and early exits.  fast on example.  incomplete on real input.

--- day16/day16.py
+++ day16/day16.py
@@ -24,19 +24,27 @@ def build_maze(filepath):
     return maze_path, maze_start, maze_end
 
 
-directions = [(1, 0), (0, -1), (-1, 0), (0, 1)]
+EAST = (0, 1)
+SOUTH = (1, 0)
+WEST = (0, -1)
+NORTH = (-1, 0)
+
+# fmt:off
+TURN_RIGHT = {
+    SOUTH: WEST, 
+    WEST: NORTH, 
+    NORTH: EAST,
+    EAST: SOUTH
+    }
 
 
-def turn_right(cur_dir):
-    return directions[
-        (directions.index(cur_dir) + 1 + len(directions)) % len(directions)
-    ]
-
-
-def turn_left(cur_dir):
-    return directions[
-        (directions.index(cur_dir) - 1 + len(directions)) % len(directions)
-    ]
+# fmt:off
+TURN_LEFT = {
+    SOUTH: EAST, 
+    EAST: NORTH, 
+    NORTH: WEST,
+    WEST: SOUTH
+    }
 
 
 def bfs_score(path, start, end):
@@ -44,18 +52,17 @@ def bfs_score(path, start, end):
 
     location_scores = {}
 
-    location_scores[start] = 0
-
-    maze_start = (*start, directions[3], 0)  # x, y, direction, score
+    location_scores[start] = (0, 0)  # score, steps
+    maze_start = (*start, (0, 1), 0)  # x, y, direction, score
     queue.append(maze_start)
 
     while queue:
         cur_x, cur_y, cur_dir, cur_score = queue.popleft()
 
         allowed_directions_and_score = [
-            (cur_dir, cur_score + 1),
-            (turn_left(cur_dir), cur_score + 1001),
-            (turn_right(cur_dir), cur_score + 1001),
+            (cur_dir, cur_score + 1),  
+            (TURN_LEFT[cur_dir], cur_score + 1001),  
+            (TURN_RIGHT[cur_dir], cur_score + 1001), 
         ]
 
         for new_dir, new_score in allowed_directions_and_score:
@@ -65,18 +72,89 @@ def bfs_score(path, start, end):
             if new_loc not in path:
                 continue
 
+            _, prev_steps = location_scores.get((cur_x, cur_y), (float('inf'), float('inf')))
+            cur_steps = prev_steps + 1
+
             if new_loc in location_scores:
-                if new_score < location_scores[new_loc]:
-                    location_scores[new_loc] = new_score
+                old_score, old_steps = location_scores[new_loc]
+                if new_score < old_score or (new_score == old_score and cur_steps < old_steps):
+                    location_scores[new_loc] = (new_score, cur_steps)
                     queue.append((*new_loc, new_dir, new_score))
             else:
-                location_scores[new_loc] = new_score
+                location_scores[new_loc] = (new_score, cur_steps)
                 queue.append((*new_loc, new_dir, new_score))
 
-    return location_scores[end]
-
+    return location_scores[end] 
 
 def part1(filepath):
     maze, start, end = build_maze(filepath)
 
-    return bfs_score(maze, start, end)
+    return bfs_score(maze, start, end)[0]
+
+
+def recursive_find_paths(
+    maze_path,
+    current,
+    end,
+    target_score,
+    target_steps,
+    current_score,
+    current_steps,
+    current_dir,
+    visited,
+    valid_locations,
+):
+    manhattan_distance = abs(current[0] - end[0]) + abs(current[1] - end[1])
+
+    if current_steps + manhattan_distance > target_steps:
+        return
+
+    if current_score > target_score:
+        return  
+
+    if current == end:
+        if current_score == target_score and current_steps == target_steps:
+            valid_locations.update(visited)  
+        return
+
+    allowed_directions_and_score = [
+        (current_dir, current_score + 1),  
+        (TURN_LEFT[current_dir], current_score + 1001),  
+        (TURN_RIGHT[current_dir], current_score + 1001),  
+    ]
+
+    for new_dir, new_score in allowed_directions_and_score:
+        new_x, new_y = current[0] + new_dir[0], current[1] + new_dir[1]
+        new_loc = (new_x, new_y)
+
+        if new_loc not in maze_path or new_loc in visited:
+            continue
+
+        recursive_find_paths(
+            maze_path,
+            new_loc,
+            end,
+            target_score,
+            target_steps,
+            new_score,
+            current_steps + 1,  
+            new_dir,
+            visited | {new_loc},  
+            valid_locations,
+        )
+
+def part2(filepath):
+    maze_path, start, end = build_maze(filepath)
+
+    target_score, steps  = bfs_score(maze_path, start, end)
+    manhattan_distance = abs(start[0]-end[0]) + abs(start[1]-end[1])
+
+    turns = (target_score - steps) // 1000
+
+    print(f"score:{target_score}; man_dist:{manhattan_distance}; steps:{steps}; turns:{turns}")
+
+    valid_locations = set()
+
+    recursive_find_paths(maze_path, current=start, end=end, target_score=target_score, target_steps=steps,  current_score=0, current_steps=0, current_dir=EAST, visited={start}, valid_locations=valid_locations)
+
+    return len(valid_locations)
--- day16/test_day16.py
+++ day16/test_day16.py
@@ -5,3 +5,9 @@ def test_part1():
     assert day16.part1("day16/input_example.txt") == 7036
     assert day16.part1("day16/input_example_2.txt") == 11048
     assert day16.part1("day16/input.txt") == 134588
+
+
+def test_part2():
+    assert day16.part2("day16/input_example.txt") == 45
+    assert day16.part2("day16/input_example_2.txt") == 64
+    # assert day16.part2("day16/input.txt") == 0
